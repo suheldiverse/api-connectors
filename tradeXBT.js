1e78f383ca1a92bad61c2a1b84cc0be222fa6bc1
suheldiverse <suhel.a@diverselynx.com>
2019-09-06 11:47:14 +0530

    chnages

--- tradeXBT.js
+++ tradeXBT.js
@@ -11,13 +11,8 @@ client.on('close', () => console.log('Connection closed.'));
 client.on('initialize', () => console.log('Client initialized, data is flowing.'));
 
 client.addStream('XBTUSD',  'trade', function(data, symbol, tableName) {
-  console.log(`Got update for ${tableName}:${symbol}. Current state:\n${JSON.stringify(data).slice(0, 100)}...`);
-  
-  console.log('data', data);
-
-  
-  
-  
+  //console.log(`Got update for ${tableName}:${symbol}. Current state:\n${JSON.stringify(data).slice(0, 100)}...`);
+  //console.log('data', data);
   //Suhel Start code to insert data into databse
    let date_ob = new Date();
    let milisecond = date_ob.getMilliseconds();  
@@ -34,20 +29,18 @@ client.addStream('XBTUSD',  'trade', function(data, symbol, tableName) {
     obj.id = '1'+id;
     return obj;
   })
-
   
    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
      if (err) throw err;
      var dbo = db.db("crypto");
      
-     dbo.collection("trade").insert(result,    function(err, res) {
+     dbo.collection("tradexbtusd").insert(result,    function(err, res) {
       if (err) throw err;
       console.log("Number of documents inserted: " + res.insertedCount);
        db.close();
      });
      
-   });
-   
+   });   
    //Suhel End code to insert data into databse  
 
 });
